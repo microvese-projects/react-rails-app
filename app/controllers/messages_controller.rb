8a97716f892a0005da851a51eea089e73f12c66b
Benson <njugunab655@gmail.com>
2023-08-21 19:46:34 +0300

    Add message view and api

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,10 @@
+class MessagesController < ApplicationController
+  def index
+    @random_message = Message.order("RANDOM()").first
+
+    respond_to do |format|
+      format.json { render json: @random_message }
+      format.html
+    end
+  end
+end
