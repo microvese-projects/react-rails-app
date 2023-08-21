1bc755035cb355cb2b73dcde3b9785d6c21e6e97
Benson <njugunab655@gmail.com>
2023-08-21 18:35:45 +0300

    Add messages table and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create([
+  { text: 'Hello, world!' },
+  { text: 'Welcome to our app.' },
+  { text: 'Nice to meet you.' },
+  { text: 'This is the beggining.' },
+  { text: 'Goodbye. See you soon.' }
+])
