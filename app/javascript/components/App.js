668b917438e8fde40c08cbb5d1ed67f2a36b324f
Benson <njugunab655@gmail.com>
2023-08-22 13:13:21 +0300

    Add redux and set store

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
 import React from 'react';
 import Home from "./Home";
 
 const App = () => {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
