75d74e6c45b4adb49f10f47b4c0267ddaa3db307
Benson <njugunab655@gmail.com>
2023-08-22 07:38:32 +0300

    Fix linters

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @random_message = Message.order("RANDOM()").first
+    @random_message = Message.order('RANDOM()').first
 
     respond_to do |format|
       format.json { render json: @random_message }
